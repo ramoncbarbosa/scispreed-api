4587a6d0d72cfd540e5062e727ffac1cad36ef0e
ramoncbarbosa <ramon.cobarbosa@gmail.com>
2026-03-26 22:58:31 -0300

    feat: init project

---
+++ src/main/java/dev/neuroncrafters/scispread_api/ScispreadApiApplication.java
@@ -0,0 +1,13 @@
+package dev.neuroncrafters.scispread_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScispreadApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScispreadApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/neuroncrafters/scispread_api/ScispreadApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.neuroncrafters.scispread_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScispreadApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
